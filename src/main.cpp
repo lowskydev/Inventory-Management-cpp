09a635dbec3412e5779cf525335c341a30ca3d72
lowskydev <lowskydev@gmail.com>
2026-03-30 17:22:47 +0200

    test: add scratch test for derived classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <memory>
+#include <vector>
+
+#include "Electronics.h"
+#include "Grocery.h"
 
 int main() {
-  std::cout << "Hello Autodesk!:)" << std::endl;
+  // Test display on derived classes
+  std::vector<std::shared_ptr<Item>> items;
+  items.push_back(
+      std::make_shared<Grocery>("G1", "Apple", 15, 1.35, "2026-03-31"));
+  items.push_back(std::make_shared<Electronics>("E1", "Phone", 1, 399.99, 24));
+
+  for (const auto& item : items) {
+    item->display();
+  }
+
   return 0;
 }
