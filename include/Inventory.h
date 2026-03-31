7835a33339bbd96653238520632c7b894f2b6a75
lowskydev <lowskydev@gmail.com>
2026-04-01 01:41:38 +0200

    feat: implement Inventory base class header

--- include/Inventory.h
+++ include/Inventory.h
@@ -1,1 +1,33 @@
 #pragma once
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "Item.h"
+
+class Inventory {
+ private:
+  std::vector<std::shared_ptr<Item>> items;
+
+ public:
+  Inventory() = default;
+  explicit Inventory(std::vector<std::shared_ptr<Item>> items);
+
+  Inventory(const Inventory&) = delete;
+  Inventory& operator=(const Inventory&) = delete;
+
+  Inventory(Inventory&&) = default;
+  Inventory& operator=(Inventory&&) = default;
+
+  void addItem(std::shared_ptr<Item> item);
+
+  void removeItem(const std::string& itemID);
+  void updateQuantity(const std::string& itemID, int quantity);
+  void displayInventory() const;
+
+  [[nodiscard]] std::shared_ptr<Item> findMostExpensive() const;
+
+  [[nodiscard]] std::vector<std::shared_ptr<Item>> belowQuantityThreshold(
+      int threshold) const;
+};
