9429a272af47d78378c3662d7c6f296cf146b2c4
lowskydev <lowskydev@gmail.com>
2026-04-01 20:31:35 +0200

    test: add scratch test for utility functions

--- src/main.cpp
+++ src/main.cpp
@@ -5,56 +5,33 @@
 #include "Electronics.h"
 #include "Grocery.h"
 #include "Inventory.h"
+#include "Utils.h"
 
 int main() {
   // create inventory
   Inventory myInv;
-  myInv.addItem(std::make_shared<Electronics>("E1", "Phone", 2, 399, 24));
-  myInv.addItem(std::make_shared<Electronics>("E2", "Laptop", 5, 690, 12));
+  myInv.addItem(std::make_shared<Electronics>("E1", "Phone", 35, 399, 24));
+  myInv.addItem(std::make_shared<Electronics>("E2", "Laptop", 9, 690, 12));
+  myInv.addItem(std::make_shared<Electronics>("E3", "USB C", 120, 12, 128));
   myInv.addItem(
       std::make_shared<Grocery>("G1", "Banana", 58, 2.95, "2026-12-14"));
+  myInv.addItem(
+      std::make_shared<Grocery>("G2", "Dragon Fruit", 7, 2.95, "2026-12-14"));
 
   std::cout << "Initial inventory: \n";
   myInv.displayInventory();
   std::cout << '\n';
 
-  // removing items
-  myInv.removeItem("A5");
-  myInv.removeItem("E2");
+  const auto lowStock = utils::filterItems(
+      myInv, [](const auto& item) { return item->getQuantity() < 10; });
 
-  // update item
-  myInv.updateQuantity("A5", 1);
-  myInv.updateQuantity("E1", 1);
-
-  std::cout << "\nAfter operations:\n";
-  myInv.displayInventory();
-
-  // most expensive
-  if (const auto mostExpensiveItem = myInv.findMostExpensive()) {
-    std::cout << "\nMost expensive Item: \n";
-
-    mostExpensiveItem->display();
-  } else {
-    std::cout << "No items in inventory\n";
-  }
-
-  // below threshold
-  std::cout << "\nThreshold below 5:\n";
-  for (const auto newInv = myInv.belowQuantityThreshold(5);
-       const auto& item : newInv) {
+  for (const auto& item : lowStock) {
     item->display();
   }
+  std::cout << '\n';
 
-  // move semantics
-  Inventory inv2 = std::move(myInv);
-  std::cout << "\nAfter move:\n";
-  inv2.displayInventory();
-
-  // empty edge case
-  std::cout << "\nEmpty inv:\n";
-  Inventory emptyInv;
-  emptyInv.findMostExpensive();
-  emptyInv.displayInventory();
+  utils::sortItems(myInv, [](const auto& item) { return item->getQuantity(); });
+  myInv.displayInventory();
 
   return 0;
 }
