0717252344c18fb4cbe9d3949d75c518f99c523e
lowskydev <lowskydev@gmail.com>
2026-04-01 14:56:36 +0200

    test: add scratch test for Inventory class

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,57 @@
 
 #include "Electronics.h"
 #include "Grocery.h"
+#include "Inventory.h"
 
 int main() {
-  // Test display on derived classes
-  std::vector<std::shared_ptr<Item>> items;
-  items.push_back(
-      std::make_shared<Grocery>("G1", "Apple", 15, 1.35, "2026-03-31"));
-  items.push_back(std::make_shared<Electronics>("E1", "Phone", 1, 399.99, 24));
+  // create inventory
+  Inventory myInv;
+  myInv.addItem(std::make_shared<Electronics>("E1", "Phone", 2, 399, 24));
+  myInv.addItem(std::make_shared<Electronics>("E2", "Laptop", 5, 690, 12));
+  myInv.addItem(
+      std::make_shared<Grocery>("G1", "Banana", 58, 2.95, "2026-12-14"));
 
-  for (const auto& item : items) {
+  std::cout << "Initial inventory: \n";
+  myInv.displayInventory();
+  std::cout << '\n';
+
+  // removing items
+  myInv.removeItem("A5");
+  myInv.removeItem("E2");
+
+  // update item
+  myInv.updateQuantity("A5", 1);
+  myInv.updateQuantity("E1", 1);
+
+  std::cout << "\nAfter operations:\n";
+  myInv.displayInventory();
+
+  // most expensive
+  if (const auto mostExpensiveItem = myInv.findMostExpensive()) {
+    std::cout << "\nMost expensive Item: \n";
+
+    mostExpensiveItem->display();
+  } else {
+    std::cout << "No items in inventory\n";
+  }
+
+  // below threshold
+  std::cout << "\nThreshold below 5:\n";
+  for (const auto newInv = myInv.belowQuantityThreshold(5);
+       const auto& item : newInv) {
     item->display();
   }
+
+  // move semantics
+  Inventory inv2 = std::move(myInv);
+  std::cout << "\nAfter move:\n";
+  inv2.displayInventory();
+
+  // empty edge case
+  std::cout << "\nEmpty inv:\n";
+  Inventory emptyInv;
+  emptyInv.findMostExpensive();
+  emptyInv.displayInventory();
 
   return 0;
 }
