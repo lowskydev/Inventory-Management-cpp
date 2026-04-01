f2e76269c3a8d80898c0679ac6ff73785a3232e3
lowskydev <lowskydev@gmail.com>
2026-04-01 21:11:20 +0200

    feat: implement inventory Exceptions

--- include/Exceptions.h
+++ include/Exceptions.h
@@ -1,1 +1,39 @@
 #pragma once
+#include <stdexcept>
+#include <string>
+
+class InventoryException : public std::runtime_error {
+ public:
+  explicit InventoryException(const std::string& message)
+      : std::runtime_error{message} {}
+};
+
+class ItemNotFoundException : public InventoryException {
+ private:
+  std::string itemID;
+
+ public:
+  explicit ItemNotFoundException(const std::string& itemID)
+      : InventoryException{"Item with ID \"" + itemID + "\" not found"},
+        itemID{itemID} {}
+
+  [[nodiscard]] const std::string& getItemID() const { return itemID; }
+};
+
+class DuplicateItemException : public InventoryException {
+ private:
+  std::string itemID;
+
+ public:
+  explicit DuplicateItemException(const std::string& itemID)
+      : InventoryException{"Item with ID \"" + itemID + "\" already exists"},
+        itemID{itemID} {}
+
+  [[nodiscard]] const std::string& getItemID() const { return itemID; }
+};
+
+class InvalidValueException : public InventoryException {
+ public:
+  explicit InvalidValueException(const std::string& message)
+      : InventoryException{message} {}
+};
